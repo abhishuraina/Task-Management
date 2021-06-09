ef53d8991bb6b080c733f2666e0261a9cac017af
abhishuraina <abhishuraina@gmail.com>
2021-06-09 16:06:54 +0530

    test config

--- project_management/urls.py
+++ project_management/urls.py
@@ -5,5 +5,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('project.urls')),
-    path('project/', include('frontend.urls'))
+    path('', include('frontend.urls'))
 ]
